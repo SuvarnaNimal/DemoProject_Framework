089a159a3cd89ceb8e9a5855bd2f61247dbc5b85
Isha <Isha@DESKTOP-IVGOSS9>
2025-06-30 19:09:45 +0530

    First Test Commit

---
+++ DemoProject_Framework/src/test/java/Test/LoginTest.java
@@ -0,0 +1,34 @@
+
+package Test;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+import Base.BaseClass;
+import PageObjects.LoginPage;
+import Utility.ReadConfig;
+
+@Listeners(listeners.TestListener.class)
+public class LoginTest extends BaseClass {
+
+    @Test(dataProvider = "loginData")
+    public void validLoginTest(String user, String pass) {
+        LoginPage lp = new LoginPage(driver);
+        test.info("Entering username");
+        lp.enterUsername(user);
+        test.info("Entering password");
+        lp.enterPassword(pass);
+        test.info("Clicking login");
+        lp.clickLogin();
+        
+        //Assert.assertTrue(false);
+    }
+
+    @DataProvider(name = "loginData")
+    public Object[][] loginData() {
+        ReadConfig config = new ReadConfig();
+        return new Object[][] {
+            { config.getUsername(), config.getPassword() }
+        };
+    }
+}
